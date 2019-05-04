1380f4a9728f88df2114428fc41be304695be04f
Duy Dn <heoconnho135135@gmail.com>
2019-05-04 18:35:50 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCDejYNk9sqXnq7l0tU4nBWegNCunKjcpA",
+  authDomain: "guide-me-239610.firebaseapp.com",
+  databaseURL: "https://guide-me-239610.firebaseio.com",
+  projectId: "guide-me-239610",
+  storageBucket: "guide-me-239610.appspot.com",
+  messagingSenderId: "577810828265",
+  appId: "1:577810828265:web:c405209156ac1c9d"
+};
+firebase.initializeApp(firebaseConfig);
