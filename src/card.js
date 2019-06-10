3dc32346dd3c717e63311958e2291d7d91efa1f6
Iamnotduy <heoconnho135135@gmail.com>
2019-06-10 21:46:46 +0700

    fully completed request action

--- src/card.js
+++ src/card.js
@@ -1,90 +1,90 @@
 function guideme_card() {
-  while (cardContainer.children[0] != null) cardContainer.children[0].remove()
+    while (cardContainer.children[0] != null) cardContainer.children[0].remove()
 
-  for (let k in userList) {
-    if (k != user.uid) showCard(userList[k]);
-  }
+    for (let k in userList) {
+        if (k != user.uid) showCard(userList[k]);
+    }
 
-  filterCard();
+    filterCard();
 
-  if (firstLoad) return;
+    if (firstLoad) return;
 
-  function showCard(target) {
-    let src = target.photoURL
-    let name = target.displayName
-    let quote = target.quote ? target.quote : 'Nice to meet you'
+    function showCard(target) {
+        let src = target.photoURL
+        let name = target.displayName
+        let quote = target.quote ? target.quote : 'Nice to meet you'
+        
+        let avatarContainer = newElement('DIV', 'avatar-container')
+        let avatar = newElement('IMG', 'avatar')
+        avatar.src = src
+        avatar.setAttribute('style', 'background: #bdc3c7')
+        avatarContainer.append(avatar)
     
-    let avatarContainer = newElement('DIV', 'avatar-container')
-      let avatar = newElement('IMG', 'avatar')
-      avatar.src = src
-      avatar.setAttribute('style', 'background: #bdc3c7')
-      avatarContainer.append(avatar)
-  
-    let infoContainer = newElement('DIV', 'info-container')
-      let _name = newElement('P', 'name', name)
-      let _quote = newElement('Q', 'quote', quote)
-      infoContainer.append(_name)
-      infoContainer.append(_quote)
-  
-    let toolbarContainer = newElement('DIV', 'toolbar')
-    let _user = newElement('DIV', 'user-container')
-    let _mes = newElement('DIV', 'mes-container')
-    let _req = newElement('DIV', 'req-container')
-    let usericon = newElement('I', 'far fa-user-circle')
-    let mesicon = newElement('I', 'fas fa-comment-dots')
-    if (!chat.loaded) {
-      mesicon.style.color = 'rgba(255, 255, 255, 0.5)';
-      chat.ready.push(() => mesicon.style.color = 'white');
-    }
-    let reqicon = newElement('I', 'fas fa-check-square')
+        let infoContainer = newElement('DIV', 'info-container')
+        let _name = newElement('P', 'name', name)
+        let _quote = newElement('Q', 'quote', quote)
+        infoContainer.append(_name)
+        infoContainer.append(_quote)
     
-    _user.append(usericon)
-    _mes.append(mesicon)
-    _req.append(reqicon)
-    toolbarContainer.append(_user)
-    toolbarContainer.append(_mes)
-    toolbarContainer.append(_req)
-    
-    _req.onclick = () => {
-      if (reqicon.style.color == 'rgba(255, 255, 255, 0.5)') return;
-      reqicon.style.color = 'rgba(255, 255, 255, 0.5)';
-      sendingRequest({type: 'req', time: new Date(), receiver: target.uid})
-    }
-    _mes.onclick = () => {
-      if (!chat.loaded) return;
-        getRoom(target.uid, (roomId, msg) => {
-          mesToChatContainer(roomId, msg, target)
-      })
-    }
+        let toolbarContainer = newElement('DIV', 'toolbar')
+        let _user = newElement('DIV', 'user-container')
+        let _mes = newElement('DIV', 'mes-container')
+        let _req = newElement('DIV', 'req-container')
+        let usericon = newElement('I', 'far fa-user-circle')
+        let mesicon = newElement('I', 'far fa-comment-dots')
+        if (!chat.loaded) {
+        mesicon.style.color = 'rgba(255, 255, 255, 0.5)';
+        chat.ready.push(() => mesicon.style.color = 'white');
+        }
+        let reqicon = newElement('I', 'far fa-paper-plane')
+        
+        _user.append(usericon)
+        _mes.append(mesicon)
+        _req.append(reqicon)
+        toolbarContainer.append(_user)
+        toolbarContainer.append(_mes)
+        toolbarContainer.append(_req)
+        
+        _req.onclick = () => {
+            if (target.type == 'visitor') reqicon.style.color = 'rgba(255, 255, 255, 0.5)';
+            if (reqicon.style.color == 'rgba(255, 255, 255, 0.5)') return;
+            reqicon.style.color = 'rgba(255, 255, 255, 0.5)';
+            sendingRequest({type: 'req', time: new Date(), receiver: target.uid})
+        }
+        _mes.onclick = () => {
+        if (!chat.loaded) return;
+            getRoom(target.uid, (roomId, msg) => {
+            mesToChatContainer(roomId, msg, target)
+        })
+        }
 
-    let modal = document.getElementById("myModal")
-    let span = document.getElementsByClassName("close")[0]
-    let targetProfile = modal.children[0].children[1]
-    _user.onclick = () => {
-      viewProfile = new Profile(target, targetProfile)
-      viewProfile.getInfo()
-      modal.style.display = "block"
-    }
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    window.addEventListener('click', function(event) {
-      if (event.target == modal) {
+        let modal = document.getElementById("myModal")
+        let span = document.getElementsByClassName("close")[0]
+        let targetProfile = modal.children[0].children[1]
+        _user.onclick = () => {
+        viewProfile = new Profile(target, targetProfile)
+        viewProfile.getInfo()
+        modal.style.display = "block"
+        }
+        span.onclick = function() {
         modal.style.display = "none";
-      }
-    })
+        }
+        window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        })
 
-    let card = newElement('DIV', 'card')
-    card.append(avatarContainer)
-    card.append(infoContainer)
-    let _div = newElement('div');
-    _div.setAttribute('cardid', target.uid);
-    _div.append(card);
-    _div.append(toolbarContainer);
-    cardContainer.append(_div);
-  }
-  
-  incProBar();
-  console.log('card.js loaded')
-  //}
+        let card = newElement('DIV', 'card')
+        card.append(avatarContainer)
+        card.append(infoContainer)
+        let _div = newElement('div');
+        _div.setAttribute('cardid', target.uid);
+        _div.append(card);
+        _div.append(toolbarContainer);
+        cardContainer.append(_div);
+    }
+    
+    incProBar();
+    console.log('card.js loaded')
 }
