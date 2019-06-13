3941f75c18235813bfcf54c96d9ad42e8e190c79
Iamnotduy <heoconnho135135@gmail.com>
2019-06-13 13:06:37 +0700

    add comment, +rate animation

--- example/app.js
+++ example/app.js
@@ -1,4 +1,23 @@
+const config = {
+	apiKey: "AIzaSyDejD_sOSrt_GSGjyxDJj40nGTYCaJ5MJI",
+	authDomain: "guideme1.firebaseapp.com",
+	databaseURL: "https://guideme1.firebaseio.com",
+	projectId: "guideme1",
+	storageBucket: "guideme1.appspot.com",
+	messagingSenderId: "283911475239",
+	appId: "1:283911475239:web:d08fa5d4aa81e905"
+}
+firebase.initializeApp(config);
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
+
 //$(() => {
+	/*
 	const form = document.getElementById('message-form');
 	const messageField = document.getElementById('message');
 	const messagesList = document.getElementById('messages');
@@ -16,7 +35,7 @@
 	})
 
 	form.onsubmit = e => {
-    e.preventDefault(); 
+	e.preventDefault(); 
 
 		var message = messageField.value;
 
@@ -26,8 +45,8 @@
 	};
 
 	reqHandler.on('Request', data => {
-    //messagesList.innerHTML += '<li class="received">' + data.data + '</li>';
-    console.log(data.sender + ': ' + data.data);
+	//messagesList.innerHTML += '<li class="received">' + data.data + '</li>';
+	console.log(data.sender + ': ' + data.data);
 	});
 
 	reqHandler.on('Close', event => {
@@ -40,3 +59,4 @@
 		reqHandler.close();
 	};
 //});
+*/
--- src/trip.js
+++ src/trip.js
@@ -4,10 +4,11 @@ function guideme_trip () {
 	let modal = $('.mymodal')[1]
 
 	window.showTripAccepted = (list = [], data) => {
+		$(modal).fadeIn()
 		modal.style.display = 'block'
-		let header = modal.children[0].children[0]
+		let header = modal.children[0].children[0] // title
 			header.children[0].innerHTML = '#Trip'
-		let main = modal.children[0].children[1]
+		let main = modal.children[0].children[1] // main
 			main.children[0].innerHTML = "Guide: "
 			main.children[1].innerHTML = "Visitor: "
 			let time = main.children[2]
@@ -18,45 +19,43 @@ function guideme_trip () {
 				}
 				else main.children[1].innerHTML += ' '+userList[uid].displayName
 			})
-			let commentForm = main.children[3]
-			let rateForm = main.children[4]
-			commentForm.style.display = 'none'
-			rateForm.style.display = 'none'	
-		let footer = modal.children[0].children[2]
+		let feedback = modal.children[0].children[2] //comment+rate
+			feedback.style.display = 'none'
+		let footer = modal.children[0].children[3] // button
 			let btnEnd = footer.children[0]
 			let btnConfirm = footer.children[1]
 			let btnCancel = footer.children[2]
 			let btnReturn = footer.children[3]
-			btnReturn.style.display = 'none'
-			btnConfirm.style.display = 'none'
-			btnCancel.style.display = 'none'
-			btnEnd.style.display = 'none'
+			$(btnReturn).css('display', 'none')
+			$(btnCancel).css('display', 'none')
+			$(btnConfirm).css('display', 'none')
+			$(btnEnd).css('display', 'none')
 			if (user.moreinfo.type == 'guide') {
-				btnReturn.style.display = 'block'
+				$(btnReturn).fadeIn()
 				btnReturn.onclick = () => {
-					modal.style.display = 'none'
+					$(modal).fadeOut()
 				}
 			}
 			else {
-				btnEnd.style.display = 'block'
+				$(btnEnd).fadeIn()
 				btnEnd.onclick = () => {
-					btnConfirm.style.display = 'block'
-					btnCancel.style.display = 'block'
-					btnEnd.style.display = 'none'
-					commentForm.style.display = 'block'
-					rateForm.style.display = 'block'
+					$(btnCancel).fadeIn()
+					$(btnConfirm).fadeIn()
+					$(btnEnd).fadeOut()
+					$(feedback).fadeIn()
+					$(main).fadeOut()
 				}
 				btnCancel.onclick = () => {
-					btnConfirm.style.display = 'none'
-					btnCancel.style.display = 'none'
-					btnEnd.style.display = 'block'
-					commentForm.style.display = 'none'
-					rateForm.style.display = 'none'
+					$(btnCancel).fadeOut()
+					$(btnConfirm).fadeOut()
+					$(btnEnd).fadeIn()
+					$(feedback).fadeOut()
+					$(main).fadeIn()
 				}
 				btnConfirm.onclick = () => {
 					$(`[reqId='${data.key}']`).fadeOut('fast', function() { this.remove(); });
 					completingRequest({type: 'completed', time: new Date(), receiver: data.target, key: data.key, comment: 'thank hihi', rate: 5})
-					modal.style.display = 'none'
+					$(modal).fadeOut()
 				}
 			}
 	}
@@ -121,7 +120,7 @@ function guideme_trip () {
 
 	window.addEventListener('click', event => {
 		if (event.target == modal) {
-			modal.style.display = 'none'
+			$(modal).fadeOut()
 		}
 	})
 
