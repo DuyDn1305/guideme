f849fda32cbacdf0e93461b2c138b2f439ef8779
Iamnotduy <heoconnho135135@gmail.com>
2019-06-15 17:14:53 +0700

    add icon, add googlemap style[5], fix itnitmap-mechanism

--- src/map.js
+++ src/map.js
@@ -1,6 +1,6 @@
 function guideme_map() {
 	if (firstLoad) return;
-	window.getUserCurrentPosition = (id) => {
+	function getUserCurrentPosition (id) {
 		if (navigator.geolocation) {
 			navigator.geolocation.watchPosition(pos => {
 				console.log(pos)
@@ -24,37 +24,76 @@ function guideme_map() {
 		infoWindow.open(map);
 	}
 	
-	window.creatingScript = (cb) => {
+	function creatingScript () {
 		let script = document.createElement('script')
 		db.ref("key").once("value", snap => {
 			key = snap.val()
 			script.src = "https://maps.googleapis.com/maps/api/js?key="+key+"&callback=initMap"
 			document.body.append(script)
-			script.onload = () => {
-				cb()
-			}
 		})
 	}
+	creatingScript()
 	
 	window.initMap = () => {
 		map = new google.maps.Map(xMap, {
 			center: {lat: 10.035, lng: 105.775},
 			zoom: 12,
+			mapTypeControl: false
 		});
+		
+        let styleControl = document.getElementById('style-selector-control');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(styleControl);
+
+        var styleSelector = document.getElementById('style-selector');
+        map.setOptions({styles: styles[styleSelector.value]});
+
+        styleSelector.addEventListener('change', function() {
+          map.setOptions({styles: styles[styleSelector.value]});
+        });
 		infoWindow = new google.maps.InfoWindow
 	
-		window.showPos = (latLng, id) => {
+		function showPos (latLng, map, id, iconURL = '../img/undefined.png') {
 			if (markers[id]) markers[id].setMap(null)
-			let marker = new google.maps.Marker({
+			var marker = new google.maps.Marker({
 				position: latLng,
-				map: map
-			})
+				map: map, 
+				icon: {
+				  url: iconURL, 
+				  size: new google.maps.Size(48, 48),
+				  origin: new google.maps.Point(0, 0),
+				  anchor: new google.maps.Point(24, 24)
+				}, 
+				shape: {
+				  coords: [20, 0, 20, 20, 0, 30, 0, 48, 48, 48, 48, 30, 28, 30, 28, 0],
+				  type: 'poly'
+				}
+			  })
 			markers[id] = marker
 			markers[id].setMap(map);
 			marker.addListener('click', e => {
 				console.log(e)
 			})
 		}
+		// handle position
+		getUserCurrentPosition(user.uid)
+		let posRef = db.ref("position/")
+		
+		posRef.on("child_added", snap => {
+			let key = snap.key;
+			if (userDataLog[key] == undefined) initUserLog(key)
+			let iconType
+			if (userList[key].isBusy) iconType = "./img/busy.png"
+			else iconType = (userList[key].moreinfo.type == "visitor") ? "./img/visitor.png" : "./img/guide.png"
+			console.log(iconType+" "+userList[key].moreinfo.type)
+			showPos(snap.val().geolocation, map, key, iconType)	
+		})
+		posRef.on("child_changed", snap => {
+			let key = snap.key;
+			let iconType
+			if (userList[key].isBusy) iconType = "./img/busy.png"
+			else iconType = (userList[key].moreinfo.type == "visitor") ? "./img/visitor.png" : "./img/guide.png"
+			showPos(snap.val().geolocation, map, key, iconType)	
+		})	
 	}
 
 	incProBar();
