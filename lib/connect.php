80f44b4d92faa58d505ea150443ca199f3ef58ca
anhvu26 <49590629+anhvu26@users.noreply.github.com>
2019-06-16 22:03:56 +0700

    Update connect.php

--- lib/connect.php
+++ lib/connect.php
@@ -1,5 +1,4 @@
 <?php
-echo 'ok';
 require_once('vendor/autoload.php');
 
 $db = new mysqli('localhost', 'root', 'happy2code', 'webchat')  or die('Falied while connecting to database');
@@ -19,4 +18,8 @@ function swap(&$x, &$y) {
 
 function vardump($var) {
 	echo '<pre>'; print_r($var); echo '<pre>';
+}
+
+if (isset($_GET['checkdb'])) {
+	vardump($db);
 }
